d01638ed6c836f7f76707c3bd6a4dba120173061
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-04-29 13:55:39 +0530

    Update AI_Agent.py

--- AI_Agent.py
+++ AI_Agent.py
@@ -35,6 +35,8 @@ Instructions:
 - DO NOT solve manually.
 - DO NOT perform any calculation.
 - Just output a valid SQL query.
+- Treat all string comparisons as CASE-INSENSITIVE
+- Use UPPER(column) for text fields and compare with uppercase values.
 - No explanation. No reasoning steps.
 
 User request: {user_query}
